1cc2870ffc399516e995c12d209612fdce4fe5a8
lgonzalezv23 <luisenano2322@gmail.com>
2024-06-11 10:42:19 -0600

    config.php added

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$db = 'mental_health_app';
+$user = 'root';
+$pass = '';
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
