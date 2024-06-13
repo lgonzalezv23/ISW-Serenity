6e0a56e371ecc99bb9c2b245b1d1416d183f93cf
lgonzalezv23 <luisenano2322@gmail.com>
2024-06-13 11:36:40 -0600

    login formateado y seguimiento de sesion activado

--- login.php
+++ login.php
@@ -13,7 +13,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $row = $result->fetch_assoc();
         if (password_verify($password, $row['password'])) {
             $_SESSION['username'] = $username;
-            echo "Login successful";
+            header("Location: welcome.php");
+            exit();
         } else {
             echo "Invalid password";
         }
@@ -26,15 +27,158 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:700,600' rel='stylesheet' type='text/css'>
+    <style>
+        body {
+            font-family: 'Open Sans', sans-serif;
+            background: #3498db;
+            margin: 0 auto;
+            width: 100%;
+            text-align: center;
+            margin: 20px 0;
+        }
+        
+        p {
+            font-size: 12px;
+            text-decoration: none;
+            color: #ffffff;
+        }
+        
+        h1 {
+            font-size: 1.5em;
+            color: #525252;
+        }
+        
+        .box {
+            background: white;
+            width: 300px;
+            border-radius: 6px;
+            margin: 0 auto;
+            padding: 0 0 70px 0;
+            border: #2980b9 4px solid;
+        }
+        
+        .email, .password {
+            background: #ecf0f1;
+            border: #ccc 1px solid;
+            border-bottom: #ccc 2px solid;
+            padding: 8px;
+            width: 250px;
+            color: #AAAAAA;
+            margin-top: 10px;
+            font-size: 1em;
+            border-radius: 4px;
+        }
+        
+        .btn {
+            background: #2ecc71;
+            width: 125px;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            color: white;
+            border-radius: 4px;
+            border: #27ae60 1px solid;
+            margin-top: 20px;
+            margin-bottom: 20px;
+            float: left;
+            margin-left: 16px;
+            font-weight: 800;
+            font-size: 0.8em;
+            cursor: pointer;
+        }
+        
+        .btn:hover {
+            background: #2CC06B;
+        }
+        
+        #btn2 {
+            float: left;
+            background: #3498db;
+            width: 125px;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            color: white;
+            border-radius: 4px;
+            border: #2980b9 1px solid;
+            margin-top: 20px;
+            margin-bottom: 20px;
+            margin-left: 10px;
+            font-weight: 800;
+            font-size: 0.8em;
+            cursor: pointer;
+        }
+        
+        #btn2:hover {
+            background: #3594D2;
+        }
+
+        #btn3 {
+            float: left;
+            background: #e74c3c;
+            width: 125px;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            color: white;
+            border-radius: 4px;
+            border: #c0392b 1px solid;
+            margin-top: 20px;
+            margin-bottom: 20px;
+            margin-left: 10px;
+            font-weight: 800;
+            font-size: 0.8em;
+            cursor: pointer;
+        }
+        
+        #btn3:hover {
+            background: #d62c1a;
+        }
+    </style>
 </head>
 <body>
     <form method="POST" action="login.php">
-        Username: <input type="text" name="username" required><br>
-        Password: <input type="password" name="password" required><br>
-        <input type="submit" value="Login">
+        <div class="box">
+            <h1>Serenity</h1>
+            <input type="text" name="username" value="username" onFocus="field_focus(this, 'username');" onblur="field_blur(this, 'username');" class="email" />
+            <input type="password" name="password" value="password" onFocus="field_focus(this, 'password');" onblur="field_blur(this, 'password');" class="password" />
+            <div class="btn" onclick="submitForm()">Sign In</div> <!-- End Btn -->
+            <div id="btn2">Sign Up</div> <!-- End Btn2 -->
+            <div id="btn3" onclick="window.location.href='index2.html'">Home</div> <!-- End Btn3 -->
+        </div> <!-- End Box -->
     </form>
+    <p>Forgot your password? <u style="color:#f1c40f;">Click Here!</u></p>
+    
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript"></script>
+    <script>
+        function field_focus(field, default_text) {
+            if(field.value == default_text) {
+                field.value = '';
+            }
+        }
+
+        function field_blur(field, default_text) {
+            if(field.value == '') {
+                field.value = default_text;
+            }
+        }
+
+        // Fade in dashboard box
+        $(document).ready(function(){
+            $('.box').hide().fadeIn(1000);
+        });
+
+        // Stop click event
+        $('a').click(function(event){
+            event.preventDefault(); 
+        });
+
+        function submitForm() {
+            document.forms[0].submit();
+        }
+    </script>
 </body>
 </html>
--- welcome.php
+++ welcome.php
@@ -10,11 +10,60 @@ echo "Welcome, " . $_SESSION['username'];
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:700,600' rel='stylesheet' type='text/css'>
+    <style>
+        body {
+            font-family: 'Open Sans', sans-serif;
+            background: #3498db;
+            margin: 0 auto;
+            width: 100%;
+            text-align: center;
+            margin: 20px 0;
+        }
+        
+        h1 {
+            font-size: 1.5em;
+            color: #ffffff;
+        }
+        
+        .box {
+            background: white;
+            width: 300px;
+            border-radius: 6px;
+            margin: 0 auto;
+            padding: 20px;
+            border: #2980b9 4px solid;
+        }
+        
+        .btn {
+            background: #2ecc71;
+            width: 125px;
+            padding-top: 5px;
+            padding-bottom: 5px;
+            color: white;
+            border-radius: 4px;
+            border: #27ae60 1px solid;
+            margin-top: 20px;
+            margin-bottom: 20px;
+            font-weight: 800;
+            font-size: 0.8em;
+            cursor: pointer;
+        }
+        
+        .btn:hover {
+            background: #2CC06B;
+        }
+    </style>
 </head>
 <body>
-    <a href="logout.php">Logout</a>
+    <div class="box">
+        <h1>Welcome, <?php echo $_SESSION['username']; ?></h1>
+        <a href="logout.php" class="btn">Logout</a>
+    </div>
 </body>
 </html>
