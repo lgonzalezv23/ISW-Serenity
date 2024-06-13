7a235477edb810b893b3a3b43df3a121c89dfe85
lgonzalezv23 <luisenano2322@gmail.com>
2024-06-13 11:39:48 -0600

    fotgot your password eliminado

--- login.php
+++ login.php
@@ -150,7 +150,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <div id="btn3" onclick="window.location.href='index2.html'">Home</div> <!-- End Btn3 -->
         </div> <!-- End Box -->
     </form>
-    <p>Forgot your password? <u style="color:#f1c40f;">Click Here!</u></p>
     
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript"></script>
     <script>
